61f23609687bd324891b17890a3f273f2bd0a79d
Frank <frankrod44@gmail.com>
2012-02-11 21:22:38 -0500

    first commit

---
+++ Platformer/Platformer/TuberculosisEnemy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Platformer
+{
+    class TuberculosisEnemy : Enemy
+    {
+        //TODO
+    }
+}
