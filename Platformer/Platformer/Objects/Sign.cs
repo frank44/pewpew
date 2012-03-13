5d039041e6cbadc9614dd84461850e54c6cb6a5f
Andy Mok <4ndypanda@gmail.com>
2012-03-13 10:56:30 -0400

    Added some object stuff

--- Platformer/Platformer/Objects/Sign.cs
+++ Platformer/Platformer/Objects/Sign.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Platformer
 {
-    class Sign
+    class Sign : Object
     {
         #region Constants
 
@@ -61,11 +63,53 @@ namespace Platformer
         #region Initialization
 
 
-        public Sign(int levelIndex, int type, int index)
+        /// <summary>
+        /// Constructor to create a new sign.
+        /// </summary>
+        public Sign(int levelIndex, int type, int index, Vector2 position)
+            : base("w.e", position)
         {
             this.levelIndex = levelIndex;
             this.type = type;
             this.index = index;
+            Damaging = false;
+            Passable = true;
+            Bouncy = false;
+        }
+
+
+        /// <summary>
+        /// Load the sprite of the sign.
+        /// </summary>
+        public override void LoadContent()
+        {
+            base.LoadContent();
+
+            //Load the bounding rectangles of each frame here.
+        }
+
+
+        #endregion
+
+
+        #region Update and Draw
+
+
+        /// <summary>
+        /// Update the object.
+        /// </summary>
+        public override void Update(GameTime gameTime)
+        {
+            this.Update(gameTime);
+        }
+
+
+        /// <summary>
+        /// Draws the object
+        /// </summary>
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch, Vector2 screen)
+        {
+            this.Draw(gameTime, spriteBatch, screen);
         }
 
 
