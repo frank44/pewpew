250267440e8ea96f9e065d38adedadd8dafe01ea
Andy Mok <4ndypanda@gmail.com>
2012-04-08 12:59:47 -0400

    See if you got this

--- Platformer/Platformer/Objects/Sign.cs
+++ Platformer/Platformer/Objects/Sign.cs
@@ -1,9 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿#region Using Statements
+using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Audio;
+using System.Collections.Generic;
+#endregion
+
 
 namespace Platformer
 {
--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 Read our Wiki 
 
-Testing
-
 https://github.com/frank44/pewpew/wiki/_pages
